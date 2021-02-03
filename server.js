de0eda95e916bdca7558027045af089656c64596
kurtp23 <kurt_phillips@live.com>
2021-02-03 13:39:42 -0800

    working add/delete notes/working api route

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 // add express server
+const express = require(`express`);
+const fs = require(`fs`);
+const path = require(`path`);
+const { nanoid } = require("nanoid");
 // create express app
+const app = express();
+const PORT = 4000;
 // include midleware to parse json
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // html routes (routes that will serve websites)
 //define landing page route
 // use index.html inside public folder
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
 // note page route
 // use notes.html inside public folder
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
+// get route to get all notes
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
+
 //API routes
 // post route to save a note
 // ADD the new note to the db.json
-// get route to get all notes
-// put route to update a note (app.put())
-// delete route to delete a note
-// invoke listen function on express app
-const express = require(`express`);
-const path = require(`path`);
-
-const app = express();
-const PORT = 4000;
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
-
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
-
-app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "db/db.json")));
-
 app.post("/api/notes", (req, res) => {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  //newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+  console.log(req.body);
+  const newNote = req.body;
+  newNote.id = nanoid();
+  const noteFile = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  noteFile.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(noteFile));
+  res.json(newNote);
+});
+// put route to update a note (app.put())
+// app.put("/notes", function (req, res) {});
+// delete route to delete a note
+
+app.delete("/api/notes/:id", (req, res) => {
+  const noteFile = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const filteredNotes = noteFile.filter((note) => note.id !== req.params.id);
+  fs.writeFileSync("db/db.json", JSON.stringify(filteredNotes));
+  res.status(200).end();
 });
 
+// invoke listen function on express app
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
